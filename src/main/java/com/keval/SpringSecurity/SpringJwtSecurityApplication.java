8fde25317b6125bf10fc27546cb165803be38db6
Keval-Luvani <keval.webosmotic@gmail.com>
2023-03-07 15:42:38 +0530

    spring jwt project added

---
+++ src/main/java/com/keval/SpringSecurity/SpringJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.keval.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtSecurityApplication.class, args);
+	}
+
+}
